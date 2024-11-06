e33ae9e9d543df46596e2f06abe6616fe4a3c4c4
yingzhu <291180782@qq.com>
2024-11-06 23:20:05 +0800

    ✨ feat: 1. 将projects文件夹的客户端项目移动到play文件夹中

---
+++ play/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@bamboo/designer/dist/style.css': path.resolve(__dirname, '../packages/designer/dist/style.css'),
+      '@bamboo/renderer/dist/style.css': path.resolve(__dirname, '../packages/renderer/dist/style.css'),
+      '@bamboo/designer': path.resolve(__dirname, '../packages/designer/src/dev.ts'),
+      '@bamboo/canvas': path.resolve(__dirname, '../packages/canvas/src/index.ts'),
+      '@bamboo/renderer': path.resolve(__dirname, '../packages/renderer/src/index.ts'),
+    },
+  },
+});
