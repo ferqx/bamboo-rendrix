950fa64495f9d096dd3d87cacdb8d12bcf74042f
chenchao <291180782@qq.com>
2024-12-24 22:38:03 +0800

    🐞 fix: 1. 修复选择器工具在选择节点时触发两次 state的问题

--- packages/canvas/src/components/tool/Selector.tsx
+++ packages/canvas/src/components/tool/Selector.tsx
@@ -10,18 +10,16 @@ export interface SelectorToolProps {
 export const Selector = ({ state }: SelectorToolProps) => {
   const size = 24;
 
-  const { x, y, visible, name, width, height, headerTop, isHeaderBottom } = useMemo(() => {
+  const { headerTop, isHeaderBottom } = useMemo(() => {
     const top = state.y - size + 2;
     const bottom = state.y + (state.height || 0) - 2;
     const calculatedHeaderTop = top - 10 < 0 ? bottom : top;
     const calculatedIsHeaderBottom = top - 10 < 0;
     return {
-      ...state,
       headerTop: calculatedHeaderTop,
       isHeaderBottom: calculatedIsHeaderBottom,
-      name: state.selectedNode?.name || '',
     };
-  }, [state.x, state.y, state.width, state.height]); // 监听元素的坐标和宽高变化即可
+  }, [state.height, state.y]); // 监听元素的坐标和宽高变化即可
 
   return (
     <>
@@ -29,23 +27,23 @@ export const Selector = ({ state }: SelectorToolProps) => {
       <div
         className="bm-selector-tool"
         style={{
-          transform: `translate(${x}px, ${y}px)`,
-          width: width,
-          height: `${height}px`,
-          visibility: visible ? 'visible' : 'hidden',
+          transform: `translate(${state.x}px, ${state.y}px)`,
+          width: state.width,
+          height: `${state.height}px`,
+          visibility: state.visible ? 'visible' : 'hidden',
         }}
       ></div>
       <div
         className={classNames('bm-selector-header-popover', { bottom: isHeaderBottom })}
         style={{
-          transform: `translate(${x}px, ${headerTop}px)`,
-          minWidth: width,
-          visibility: visible ? 'visible' : 'hidden',
+          transform: `translate(${state.x}px, ${headerTop}px)`,
+          minWidth: state.width,
+          visibility: state.visible ? 'visible' : 'hidden',
         }}
       >
         <div className="bm-selector-actions">
           <div className="bm-selector-action-item" style={{ padding: '0 8px' }}>
-            {name}
+            {state.selectedNode?.name}
           </div>
         </div>
       </div>
--- packages/canvas/src/hooks/useTool.ts
+++ packages/canvas/src/hooks/useTool.ts
@@ -274,7 +274,7 @@ export function useTool(options?: CanvasToolOptions) {
         }
       });
     });
-    iframeWindow.addEventListener('resize', selectorTool.updateSelectorTool);
+    iframeWindow.addEventListener('resize', () => selectorTool.updateSelectorTool());
     // 监听外部的组件拖动，并清除画布中的状态
     window.addEventListener('dragenter', clearState);
     window.addEventListener('mouseover', clearState);
@@ -334,10 +334,6 @@ export function useTool(options?: CanvasToolOptions) {
     (targetNode.el as HTMLElement)?.querySelector('.drag-hover')?.classList.remove('drag-hover');
   };
 
-  const onDelete = () => {
-    selectorTool.state.selectedNode?.remove();
-  };
-
   const destroy = () => {
     window.removeEventListener('dragenter', clearState);
     window.removeEventListener('mouseover', clearState);
@@ -345,7 +341,6 @@ export function useTool(options?: CanvasToolOptions) {
 
   return {
     initEvent,
-    onDelete,
     clearState,
     destroy,
     isDragIng,
